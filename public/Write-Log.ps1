12372d0f95c9c64f960f567cfea97880345cfafb
Tim Burkholder <it@blessingsofhope.com>
2023-07-28 15:07:42 -0400

    Assembling message.

--- public/Write-Log.ps1
+++ public/Write-Log.ps1
@@ -38,4 +38,25 @@ Function Write-Log {
             HelpMessage='Syslog facility number'
             )][Int32]$SyslogFacility
     )
+
+    $SEVERITY_KEYWORD_LUT = @{
+        0='EMERG';
+        1='ALERT';
+        2='CRIT';
+        3='ERR';
+        4='WARNING';
+        5='NOTICE';
+        6='INFO';
+        7='DEBUG';
+    }
+
+    $logKeyword = $SEVERITY_KEYWORD_LUT[$LogSeverity]
+
+    # ============================================================================
+    # Assemble message.
+    $timestamp = (Get-Date).ToString('yyyy-MM-ddTHH.mm.ss')
+
+    $message = "$timestamp [$($logKeyword)] $($LogMessage)"
+
+    write-host $message
 }
