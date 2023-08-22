6f2d9e270deabc1140a645f0ceb69e0e9ad7fc6e
Tim Burkholder <it@blessingsofhope.com>
2023-08-22 11:23:39 -0400

    Added check for Posh-SYSLOG install.

--- Write-Logger.psm1
+++ Write-Logger.psm1
@@ -2,11 +2,11 @@
 .SYNOPSIS
     Send log messages to console, file, and/or syslog server.
 .DESCRIPTION
-    Send log messages to console, file and/or syslog server. 
-    * Different log levels can be specified for each destination. 
-    * Destination log levels default to -1, which disables logging for that 
+    Send log messages to console, file and/or syslog server.
+    * Different log levels can be specified for each destination.
+    * Destination log levels default to -1, which disables logging for that
         destination.
-    * There a lot of parameters, so normally I would define a hash table 
+    * There a lot of parameters, so normally I would define a hash table
         at the top of a script, and use parameter splatting.
 .EXAMPLE
     # Call from console. Useful for testing.
@@ -15,25 +15,33 @@
     # Use for script logging.
     # Top of the script.
     $logArgs = @{
-        ConsoleLogLevel = 7     # Debug 
-        FileLogLevel = 6        # Info 
+        ConsoleLogLevel = 7     # Debug
+        FileLogLevel = 6        # Info
         SyslogLevel = 4         # Warning
         LogFilePath = 'mypath.log'
         SyslogServer = 'server.host'
         SyslogPort = 514
         SyslogFacility = 10
     }
-    
+
     # Later in the script.
     Write-Logger -LogMessage 'My info message' -LogLevel 6 $logArgs
-    
+
     ...
 
     Write-Logger -LogMessage 'My error message' -LogLevel 3 $logArgs
 
 .NOTES
-    Depends on PoSH-Syslog to send syslog message.
+    Depends on PoSH-Syslog to send syslog messages.
 #>
+
+# ============================================================================
+# Check for depedency.
+if (-not (Get-Module -ListAvailable -Name Posh-SYSLOG)) {
+    Write-Host 'Please install the Posh-SYSLOG module.'
+    Exit-PSHostProcess
+}
+
 Function Write-Logger {
     [CmdletBinding()]
     Param(
